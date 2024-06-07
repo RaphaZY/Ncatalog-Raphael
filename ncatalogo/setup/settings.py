6e64279263a59c14246cb8074fd4329e4ae5b01e
RaphaZY <raphacoelho3790@gmail.com>
2024-06-06 22:18:06 -0300

    like

--- ncatalogo/setup/settings.py
+++ ncatalogo/setup/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ncatalogo',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': '3306',
     }
---
+++ ncatalogo/static/assets/js/scripts.js
@@ -0,0 +1,7 @@
+function alteraimg(){
+    var div = document.getElementById('like').src;
+    var id = document.getElementById('like').id;
+    
+    div.innerHTML = "<img src='/static/assets/img/like2.png' width='40px' height='40px'>";
+
+}
